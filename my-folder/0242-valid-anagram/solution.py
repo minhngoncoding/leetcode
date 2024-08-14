9d1a98a925813f5ae358dab75891fe3fd6cf11b6
minhngonmcoding <minhngon.dang1@gmail.com>
2024-08-14 10:17:48 +0000

    [LeetCode Sync] Runtime - 63 ms (6.69%), Memory - 16.9 MB (45.15%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,26 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hash_map = {}
-
-        for char in s:
-            if char not in hash_map:
-                hash_map[char] = 1
-            else:
-                hash_map[char] += 1
-
-        for char in t:
-            if char not in hash_map:
-                return False
-            
-            if char in hash_map:
-                if hash_map[char] == 0:
-                    return False
-                else:
-                    hash_map[char] -= 1
-                    if hash_map[char] == 0:
-                        hash_map.pop(char)
-
-        if hash_map:
-            return False
-        return True
+        char_count = {}
         
+        for char in s:
+            if char not in char_count:
+                char_count[char] = 1
+            else:
+                char_count[char] = char_count[char] + 1
+        
+        for char in t:
+            if char not in char_count or char_count[char] == 0:
+                return False
+            else:
+                char_count[char] -= 1
+                if char_count[char] == 0:
+                    char_count.pop(char)
+
+        return False if char_count else True
+
+        
+
+
+
