8bb768c86daaf1be0a0fafd1063bff47b8bb4fcb
minhngonmcoding <minhngon.dang1@gmail.com>
2024-08-14 10:33:52 +0000

    [LeetCode Sync] Runtime - 188 ms (71.86%), Memory - 18 MB (66.56%)

--- my-folder/0792-binary-search/solution.py
+++ my-folder/0792-binary-search/solution.py
@@ -2,18 +2,15 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         low = 0
         high = len(nums) - 1
-        mid = (high + low) // 2
+
         while low <= high:
-            if nums[mid] < target:
-                low = mid + 1
-            elif nums[mid] > target:
+            mid = (high + low) // 2
+
+            if nums[mid] > target:
                 high = mid - 1
+            elif nums[mid] < target:
+                low = mid + 1
             else:
                 return mid
- 
-            mid = (high + low) // 2
-    
+
         return -1
-
-
-        
