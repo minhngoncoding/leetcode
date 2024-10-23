0b8e91a68d9527d83de0152047fe2c489f81092e
minhngonmcoding <minhngon.dang1@gmail.com>
2024-10-23 07:34:38 +0000

    [LeetCode Sync] Runtime - 129 ms (5.15%), Memory - 34.6 MB (6.08%)

--- my-folder/0208-implement-trie-prefix-tree/solution.py
+++ my-folder/0208-implement-trie-prefix-tree/solution.py
@@ -1,42 +1,52 @@
 class TrieNode:
-    def __init__(self):
-        self.end_of_word = False
-        self.children = {}
+    def __init__(self,isEndNode=False):
+        self.children = [None]*26
+        self.isEndNode = isEndNode
+
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        node = self.root
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.end_of_word = True
+        current = self.root
 
-    def search(self, word: str) -> bool:
-        node = self.root
         for char in word:
-            if char not in node.children:
+            
+            index = ord(char) - ord("a")
+            print(char, index)
+            if current.children[index] is None:
+                current.children[index] = TrieNode()
+
+            current = current.children[index]
+        current.isEndNode = True
+             
+    def search(self, word: str) -> bool:
+        current = self.root
+        for char in word:
+            index = ord(char) - ord("a")
+
+            if current.children[index] is None:
                 return False
-            node = node.children[char]
-        if node.end_of_word:
-            return True
-        else: 
-            return False
+            current = current.children[index]
+        
+        return True if current.isEndNode else False
+                
 
     def startsWith(self, prefix: str) -> bool:
-        node = self.root
+        current = self.root
         for char in prefix:
-            if char not in node.children:
-                return False
-            node = node.children[char]
-        return True
+            index = ord(char) - ord("a")
 
+            if current.children[index] is None:
+                return False
+            current = current.children[index]
+
+        return True
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
-# obj.insert(word)
+# obj.insert("apple")
+# print(obj.root.children)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
